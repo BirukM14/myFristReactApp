97cc58ae1d0f5bff4d33652a2e176354b0b87d3a
BirukM14 <gbiruka14@gmail.com>
2024-10-29 00:09:58 +0300

    src\calulator.js

---
+++ src/calulator.js
@@ -0,0 +1,15 @@
+import { renderIntoDocument } from "react-dom/test-utils"
+
+function add(a,b){
+    return a+b
+
+}
+function substract(a,b){
+    return a-b
+
+}
+function divide(a,b){
+    return a/b
+
+}
+export {add,substract,divide}
